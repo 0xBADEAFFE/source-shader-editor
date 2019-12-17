db9bbb7b44cd87c0974b4248d962dd7d09d64b3a
DankParrot <dankparrot1@gmail.com>
2019-12-16 21:08:54 -0800

    Fix editor loading editor only materials when inactive

--- src/shadereditor/nodes/vbasenode.cpp
+++ src/shadereditor/nodes/vbasenode.cpp
@@ -48,7 +48,8 @@ CBaseNode::CBaseNode( const char *opName, CNodeView *p )
 
 	m_iUniqueIndexCount++;
 
-	SetupVguiTex( m_iPreviewMaterial, "shadereditor/_rtdebug_pp0" );
+	if (IsInEditMode())
+		SetupVguiTex( m_iPreviewMaterial, "shadereditor/_rtdebug_pp0" );
 	SetName( opName );
 }
 
